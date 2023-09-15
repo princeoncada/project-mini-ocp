0489e0c4d1b8f95495585ae714fafa71bb05cd89
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 04:43:56 +0800

    created migration seed file for tbl_mc_statuses

---
+++ src/main/resources/db/migration/V2_11__seed_mc_statuses.sql
@@ -0,0 +1,8 @@
+-- Version 2.11
+-- Seed tbl_mc_statuses table
+
+-- Insert Entries
+INSERT INTO tbl_mc_statuses (id, name) VALUES
+(UUID(), 'For Approval'),
+(UUID(), 'Approved'),
+(UUID(), 'Not Approved');
