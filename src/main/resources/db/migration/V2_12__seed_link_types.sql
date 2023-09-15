1452d1f56fa3d729ab11f05a57513f383cebfa8a
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 04:44:01 +0800

    created migration seed file for tbl_link_types

---
+++ src/main/resources/db/migration/V2_12__seed_link_types.sql
@@ -0,0 +1,12 @@
+-- Version 2.12
+-- Seed tbl_link_types table
+
+-- Insert Entries
+INSERT INTO tbl_link_types (id, type) VALUES
+(UUID(), 'Facebook'),
+(UUID(), 'Instagram'),
+(UUID(), 'Pinterest'),
+(UUID(), 'Twitter'),
+(UUID(), 'LinkedIn'),
+(UUID(), 'YouTube'),
+(UUID(), 'Website');
