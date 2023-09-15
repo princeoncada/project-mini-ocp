165ac3ba7f1d8a9f5acea86b015c7507e17dc3cc
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 04:42:56 +0800

    created migration seed file for tbl_departments

---
+++ src/main/resources/db/migration/V2_2__seed_departments.sql
@@ -0,0 +1,10 @@
+-- Version 2.2
+-- Seed tbl_departments table
+
+-- Insert Entries
+INSERT INTO tbl_departments (id, name, abbr) VALUES
+(UUID(), 'College of Computer and Information Science', 'CCIS'),
+(UUID(), 'College of Arts and Science', 'CAS'),
+(UUID(), 'College of Engineering and Architecture', 'CEA'),
+(UUID(), 'Alfonso T. Yuchengco College of Business', 'ATYCB'),
+(UUID(), 'Senior High School', 'SHS');
