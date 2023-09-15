80044150279f1ba4becec7ba3e9d1d5672378fda
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 04:43:34 +0800

    created migration seed file for tbl_company_categories

---
+++ src/main/resources/db/migration/V2_7__seed_company_categories.sql
@@ -0,0 +1,10 @@
+-- Version 2.7
+-- Seed tbl_company_categories table
+
+-- Insert Entries
+INSERT INTO tbl_company_categories (id, name) VALUES
+(UUID(), 'Domestic Firm'),
+(UUID(), 'International Firm'),
+(UUID(), 'Global Firm'),
+(UUID(), 'Others'),
+(UUID(), 'N/A');
