5a71558bde93e5e8fba14f3a0f0c992c8ef213cd
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 05:49:12 +0800

    refactored code in tbl_phil_provinces setting prov_code into non-unique

--- src/main/resources/db/migration/V1_17__create_phil_provinces_table.sql
+++ src/main/resources/db/migration/V1_17__create_phil_provinces_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tbl_phil_provinces
 (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
-    prov_code VARCHAR(255) NOT NULL UNIQUE,
+    prov_code VARCHAR(255) NOT NULL,
     reg_code VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
