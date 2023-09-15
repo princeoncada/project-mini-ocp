fd92ab24c434f877f6589c385c8e2a1bf1d7ad86
Prince Oncada <prince.oncada@gmail.com>
2023-09-16 05:50:11 +0800

    created migration seed file for tbl_users

---
+++ src/main/resources/db/migration/V2_21__seed_users.sql
@@ -0,0 +1,6 @@
+-- Version 2.21
+-- Seed tbl_users table
+
+-- Insert Entries
+INSERT INTO tbl_users (id, email, first_name, last_name, student_id, designation) VALUES
+(UUID(), 'dummyaccount@gmail.com', 'Dummy', 'Account', '2020103210', 'CS');
