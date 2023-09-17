d1fd8db85c7b7ec9c1abd832e484a89547203df0
KuuShyn <magalonakaj@gmail.com>
2023-09-17 12:06:01 +0800

    BUILD: api requests are accessible to all users for now temporarily

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
@@ -1,5 +1,41 @@
 package com.mmcm.projectocp.backend.spring.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 // TODO: Add security configuration here Auths and Stuff
-class SecurityConfig {
+// NOTES: This is a temporary security config for testing purposes
+// I magic ra nako na later
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+//    private val jwtRequestFilter: JwtRequestFilter
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf{
+                it.disable()
+            }
+            .authorizeHttpRequests {
+                it.requestMatchers("/user/**").permitAll()
+
+            }
+            .build()
+    }
+
+
 }
