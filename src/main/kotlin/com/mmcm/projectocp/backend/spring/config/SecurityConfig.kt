eb2e1b7cc06ab03498d38cbd206008ba838d1c5c
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 03:15:05 +0800

    removed unnecessary code in SecurityConfig.kt again

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
@@ -3,7 +3,6 @@ package com.mmcm.projectocp.backend.spring.config
 import com.mmcm.projectocp.backend.spring.domain.service.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -18,11 +17,9 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
-        private val customUserDetailsService: CustomUserDetailsService,
-) {
+class SecurityConfig{
 
-    private final val encoder: PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private final val encoder: PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
     val user: UserDetails = User
         .withUsername("user")
