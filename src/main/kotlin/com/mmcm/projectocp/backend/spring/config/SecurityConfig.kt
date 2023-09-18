1d5c199af19f15bae9fe545a07055aecb8fb6f6f
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 03:08:46 +0800

    refactored SecurityConfig.kt to only accept OAuth Logins through Google

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
@@ -7,23 +7,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val customUserDetailsService: CustomUserDetailsService
+        private val customUserDetailsService: CustomUserDetailsService,
 ) {
 
     private final val encoder: PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -63,38 +59,15 @@ class SecurityConfig(
     @Bean
     fun configure(http: HttpSecurity): SecurityFilterChain {
         http.invoke {
-            oauth2Login {
-                userInfoEndpoint {
-                    userAuthoritiesMapper = grantedAuthoritiesMapper()
-                }
-            }
+            oauth2Login { }
             csrf { disable() }
             authorizeRequests {
-                authorize("/", authenticated)
-//                authorize("/api/user/**" , hasRole("admin"))
+//                authorize("/", authenticated)
+                authorize("/api/user/**" , hasRole("admin"))
             }
-            formLogin {  }
             httpBasic {  }
             logout {  }
         }
         return http.build()
-    }
-
-    private fun grantedAuthoritiesMapper(): GrantedAuthoritiesMapper {
-        return GrantedAuthoritiesMapper { authorities ->
-            val mappedAuthorities = mutableListOf<GrantedAuthority>()
-            mappedAuthorities.add(GrantedAuthority {"admin"})
-            for (authority in authorities) {
-                when (authority) {
-                    is OidcUserAuthority ->
-                        mappedAuthorities.add(OidcUserAuthority("OIDC_USER", authority.idToken, authority.userInfo))
-                    is OAuth2UserAuthority ->
-                        mappedAuthorities.add(OAuth2UserAuthority("OAUTH2_USER", authority.attributes))
-                    else -> mappedAuthorities.add(authority)
-                }
-            }
-
-            mappedAuthorities
-        }
     }
 }
