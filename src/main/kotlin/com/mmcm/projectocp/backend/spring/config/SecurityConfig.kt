808ba640c612421b0cfaf16cbf5cf04395a3312e
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 03:15:30 +0800

    removed unnecessary code in SecurityConfig.kt again

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package com.mmcm.projectocp.backend.spring.config
 
-import com.mmcm.projectocp.backend.spring.domain.service.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
