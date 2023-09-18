3591ad761d669ec4800d141ccbdbdbb334b147d5
Prince Oncada <prince.oncada@gmail.com>
2023-09-18 20:27:47 +0800

    created Role Repository for tbl_roles

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/RoleRepository.kt
@@ -0,0 +1,12 @@
+package com.mmcm.projectocp.backend.spring.domain.repository
+
+import com.mmcm.projectocp.backend.spring.domain.model.Role
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface RoleRepository: JpaRepository<Role, String> {
+    fun findByName(name: String): Role
+}
