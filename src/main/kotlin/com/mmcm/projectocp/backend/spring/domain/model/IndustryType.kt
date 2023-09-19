634018fdeb3776af53b55f69e9aa8b02e45bb607
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 16:03:40 +0800

    Build: created IndustryType model for tbl_industry_types

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/IndustryType.kt
@@ -0,0 +1,23 @@
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+@Entity
+@Table(name = "tbl_industry_types")
+data class IndustryType(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @Column(name = "type", length = 255, nullable = false, unique = true)
+    val type: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
