3660ff8d26f2bd7b1f51c9423b31a89287e89f19
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 16:07:29 +0800

    Model: created McStatus model for tbl_mc_statuses

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/McStatus.kt
@@ -0,0 +1,24 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_mc_statuses")
+data class McStatus(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @Column(name = "name", length = 255, nullable = false, unique = true)
+    val name: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
