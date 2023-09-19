ea93445b8476aac08ba946f3d72686daa1dcf48b
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 16:02:40 +0800

    Build: created ContentType model for tbl_content_types

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/ContentType.kt
@@ -0,0 +1,24 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_content_types")
+data class ContentType(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @Column(name = "type", length = 255, nullable = false, unique = true)
+    val type: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
