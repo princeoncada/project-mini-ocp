895a19ef2f2ef3c790dc4310ac9d45d79f323e1a
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 16:08:48 +0800

    Model: created PhilippineRegion model for tbl_phil_regions

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/PhilippineRegion.kt
@@ -0,0 +1,27 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_phil_regions")
+data class PhilippineRegion(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @Column(name = "reg_code", length = 255, nullable = false, unique = true)
+    val regionCode: String,
+
+    @Column(name = "name", length = 255, nullable = false)
+    val name: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
