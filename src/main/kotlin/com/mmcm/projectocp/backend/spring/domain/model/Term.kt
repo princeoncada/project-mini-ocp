9fd7df794e143ff8490ace0c07e2fbbcd39b1d6d
Prince Oncada <prince.oncada@gmail.com>
2023-09-19 16:09:07 +0800

    Model: created Term model for tbl_terms

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/Term.kt
@@ -0,0 +1,24 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_terms")
+data class Term(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @Column(name = "code", length = 255, nullable = false, unique = true)
+    val code: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
