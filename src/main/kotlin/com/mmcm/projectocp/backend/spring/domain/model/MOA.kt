04235f387bfb0c4e51ee6fefc83012d5fb188cb0
Prince Oncada <prince.oncada@gmail.com>
2023-09-21 04:22:17 +0800

    Model: created MOA model for tbl_moas

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/MOA.kt
@@ -0,0 +1,47 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import java.time.LocalDate
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_moas")
+data class MOA(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", referencedColumnName = "id", nullable = false)
+    val branch: CompanyBranch,
+
+    @Column(name = "title", length = 255)
+    val title: String?,
+
+    @Column(name = "start_date")
+    val startDate: LocalDate?,
+
+    @Column(name = "expiry_date")
+    val expiryDate: LocalDate?,
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    val description: String?,
+
+    @Column(name = "vpaa_submission")
+    val vpaaSubmission: LocalDate?,
+
+    @Column(name = "is_active")
+    val isActive: Boolean = false,
+
+    @Column(name = "is_hiring", nullable = false)
+    val isHiring: Boolean = false,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
