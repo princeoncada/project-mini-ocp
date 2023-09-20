71c1aa21ec54364fbe90de710e6a425dfcfd7a6f
Prince Oncada <prince.oncada@gmail.com>
2023-09-21 04:22:34 +0800

    Model: created MOAPartnershipType model for tbl_moa_partnership_types

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/MOAPartnershipType.kt
@@ -0,0 +1,29 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "tbl_moa_partnership_types")
+data class MOAPartnershipType(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "moa_id", referencedColumnName = "id", nullable = false)
+    val moa: MOA,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "partnership_type_id", referencedColumnName = "id", nullable = false)
+    val partnershipType: PartnershipType,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
