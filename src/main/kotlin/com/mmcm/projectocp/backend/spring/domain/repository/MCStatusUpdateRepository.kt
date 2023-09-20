e19319781a95795dbba766856bea508350ecb2af
Prince Oncada <prince.oncada@gmail.com>
2023-09-21 06:23:24 +0800

    Repository: created repository for MCStatusUpdate model

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/MCStatusUpdateRepository.kt
@@ -0,0 +1,8 @@
+package com.mmcm.projectocp.backend.spring.domain.repository
+
+import com.mmcm.projectocp.backend.spring.domain.model.MCStatusUpdate
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MCStatusUpdateRepository : JpaRepository<MCStatusUpdate, String> {
+    // You can add custom query methods here if needed
+}
