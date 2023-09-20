709f9be46ee45a9d179681b28b10a276cb02cd78
Prince Oncada <prince.oncada@gmail.com>
2023-09-21 04:23:15 +0800

    Model: created Program model for tbl_programs

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/Program.kt
@@ -0,0 +1,31 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_programs")
+data class Program(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", referencedColumnName = "id", nullable = false)
+    val department: Department,
+
+    @Column(name = "name", length = 255, nullable = false, unique = true)
+    val name: String,
+
+    @Column(name = "abbr", length = 255, nullable = false, unique = true)
+    val abbr: String,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
