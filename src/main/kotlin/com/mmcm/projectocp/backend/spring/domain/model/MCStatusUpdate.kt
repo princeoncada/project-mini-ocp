29078e33865b999439b31e673a4f56ae13663491
Prince Oncada <prince.oncada@gmail.com>
2023-09-21 04:21:57 +0800

    Model: created MCStatusUpdate model for tbl_mc_status_updates

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/model/MCStatusUpdate.kt
@@ -0,0 +1,36 @@
+package com.mmcm.projectocp.backend.spring.domain.model
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+import java.time.LocalDate
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_mc_status_updates")
+data class MCStatusUpdate(
+    @Id
+    @Column(name = "id", length = 36, nullable = false)
+    val id: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mc_status_id", referencedColumnName = "id", nullable = false)
+    val mcStatus: MCStatus,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "moa_id", referencedColumnName = "id", nullable = false)
+    val moa: MOA,
+
+    @Column(name = "notes", columnDefinition = "TEXT")
+    val notes: String?,
+
+    @Column(name = "mc_approved_date")
+    val mcApprovedDate: LocalDate?,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: Instant
+)
