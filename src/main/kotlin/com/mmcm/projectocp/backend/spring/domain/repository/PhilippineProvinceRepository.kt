7456f7165f97a224c4af6cda158e23588f69151d
KuuShyn <magalonakaj@gmail.com>
2023-09-21 21:16:19 +0800

    Build: Philippine Api i just merged Regiom, Province, Municipality, and Barangay into one controller

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PhilippineProvinceRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PhilippineProvinceRepository.kt
@@ -3,8 +3,10 @@ package com.mmcm.projectocp.backend.spring.domain.repository
 import com.mmcm.projectocp.backend.spring.domain.model.PhilippineProvince
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface PhilippineProvinceRepository : JpaRepository<PhilippineProvince, String> {
     // You can add custom query methods here if needed
+    fun findByProvinceCode(provcode: String): Optional<PhilippineProvince>
 }
