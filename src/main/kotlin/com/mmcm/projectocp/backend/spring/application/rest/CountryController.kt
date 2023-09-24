21aea8fd0d6d7c981c2c0579134cb554f22bc695
KuuShyn <magalonakaj@gmail.com>
2023-09-24 20:21:09 +0800

    Build: refactored Country api

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/application/rest/CountryController.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/application/rest/CountryController.kt
@@ -1,51 +1,69 @@
 package com.mmcm.projectocp.backend.spring.application.rest
 
-import com.mmcm.projectocp.backend.spring.application.dto.CountryDTO
-import com.mmcm.projectocp.backend.spring.domain.model.Country
-import com.mmcm.projectocp.backend.spring.domain.repository.CountryRepository
+import com.mmcm.projectocp.backend.spring.application.dto.CountryDTOs
 import com.mmcm.projectocp.backend.spring.domain.service.CountryService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/country")
+@RequestMapping("/api/countries")
 class CountryController(
-    private val countryRepository: CountryRepository,
     private val countryService: CountryService
-) {
-    @GetMapping("/get-countries")
-    fun getCountries(pageable: Pageable) : Page<CountryDTO> {
-        return countryRepository.findAll(pageable).map { CountryDTO(it.id, it.isoCode, it.name) }
+): EntityController<CountryDTOs.GetResult, CountryDTOs.PostRequest, CountryDTOs.PutRequest> {
+    override fun getEntities(
+        pageable: Pageable
+    ): ResponseEntity<Page<CountryDTOs.GetResult>> {
+        return try {
+            val countries = countryService.getEntities(pageable)
+            ResponseEntity.ok(countries)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
     }
 
-    @GetMapping("/get-countries", params = ["countries", "iso-code"])
-    fun getCountriesByNameAndIsoCode(
-        @RequestParam("countries") countries: String,
-        @RequestParam("iso-code") isoCode: String,
-    ) : List<Country> {
-        return countryRepository.findByNameAndIsoCode(countries, isoCode)
+    override fun getEntityById(id: String, pageable: Pageable): ResponseEntity<Page<CountryDTOs.GetResult>> {
+        return try {
+            val country = countryService.getEntityById(id, pageable)
+            ResponseEntity.ok(country)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
     }
 
-    //Not working due to IsoCode or semethjing in the model
-    @PostMapping("/create-country")
-    fun createCountry(@RequestBody countryDto: CountryDTO) : CountryDTO {
-        return countryService.createCountry(countryDto)
+    override fun createEntity(
+        entityRequest: CountryDTOs.PostRequest,
+        pageable: Pageable
+    ): ResponseEntity<Page<CountryDTOs.GetResult>> {
+        return try {
+            val country = countryService.createEntity(entityRequest, pageable)
+            ResponseEntity.ok(country)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
     }
 
-    @PutMapping("/update-country/id={id}")
-    fun updateCountry(
-        @PathVariable("id") id: String,
-        @RequestBody countryDto: CountryDTO
-    ) : CountryDTO {
-        return countryService.updateCountryById(id, countryDto)
+    override fun updateEntityById(
+        id: String,
+        entityRequest: CountryDTOs.PutRequest,
+        pageable: Pageable
+    ): ResponseEntity<Page<CountryDTOs.GetResult>> {
+        return try {
+            val country = countryService.updateEntityById(id, entityRequest, pageable)
+            ResponseEntity.ok(country)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
     }
 
-    @DeleteMapping("/delete-country/id={id}")
-    fun deleteCountry(
-        @PathVariable("id") id: String) {
-        countryService.deleteCountryById(id)
+    override fun deleteEntityById(id: String, pageable: Pageable): ResponseEntity<Page<CountryDTOs.GetResult>> {
+        return try {
+            val country = countryService.deleteEntityById(id, pageable)
+            ResponseEntity.ok(country)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
     }
-
-
 }
--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/CountryRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/CountryRepository.kt
@@ -1,6 +1,8 @@
 package com.mmcm.projectocp.backend.spring.domain.repository
 
 import com.mmcm.projectocp.backend.spring.domain.model.Country
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -9,11 +11,6 @@ import java.util.*
 interface CountryRepository : JpaRepository<Country, String> {
     // You can add custom query methods here if needed
 
-    fun findByName(name: String): List<Country>
 
-    fun findByIsoCode(isoCode: String): List<Country>
-
-    fun findByNameAndIsoCode(name: String, isoCode: String): List<Country>
-
-    fun findAllByOrderByNameAsc(): List<Country>
+    fun findById(id: String, pageable: Pageable): Page<Country>
 }
