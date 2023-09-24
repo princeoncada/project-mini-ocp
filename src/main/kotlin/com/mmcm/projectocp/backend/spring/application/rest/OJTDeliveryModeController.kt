341f62436da6f4cd79b4212954820da928ad588b
KuuShyn <magalonakaj@gmail.com>
2023-09-24 20:25:49 +0800

    Build: created @OJTDeliveryMode api

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/application/rest/OJTDeliveryModeController.kt
@@ -0,0 +1,83 @@
+package com.mmcm.projectocp.backend.spring.application.rest
+
+import com.mmcm.projectocp.backend.spring.application.dto.OJTDeliveryModeDTOs
+import com.mmcm.projectocp.backend.spring.domain.service.OJTDeliveryModeService
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/ojt-delivery-mode")
+class OJTDeliveryModeController (
+    private val ojtDeliveryModeService: OJTDeliveryModeService
+) : EntityController<OJTDeliveryModeDTOs.GetResult, OJTDeliveryModeDTOs.PostRequest, OJTDeliveryModeDTOs.PutRequest>{
+
+    @GetMapping
+    override fun getEntities(pageable: Pageable): ResponseEntity<Page<OJTDeliveryModeDTOs.GetResult>> {
+        return try {
+            val ojtDeliveryMode = ojtDeliveryModeService.getEntities(pageable)
+            ResponseEntity.ok(ojtDeliveryMode)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping("/{id}")
+    override fun getEntityById(
+        @PathVariable id: String,
+        pageable: Pageable): ResponseEntity<Page<OJTDeliveryModeDTOs.GetResult>> {
+        return try {
+            val ojtDeliveryMode = ojtDeliveryModeService.getEntityById(id, pageable)
+            ResponseEntity.ok(ojtDeliveryMode)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping
+    override fun createEntity(
+        @RequestBody entityRequest: OJTDeliveryModeDTOs.PostRequest,
+        pageable: Pageable
+    ): ResponseEntity<Page<OJTDeliveryModeDTOs.GetResult>> {
+        return try {
+            val ojtDeliveryMode = ojtDeliveryModeService.createEntity(entityRequest, pageable)
+            ResponseEntity.ok(ojtDeliveryMode)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PutMapping("/{id}")
+    override fun updateEntityById(
+        @PathVariable id: String,
+        @RequestBody entityRequest: OJTDeliveryModeDTOs.PutRequest,
+        pageable: Pageable
+    ): ResponseEntity<Page<OJTDeliveryModeDTOs.GetResult>> {
+        return try {
+            val ojtDeliveryMode = ojtDeliveryModeService.updateEntityById(id, entityRequest, pageable)
+            ResponseEntity.ok(ojtDeliveryMode)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    override fun deleteEntityById(
+        @PathVariable id: String,
+        pageable: Pageable
+    ): ResponseEntity<Page<OJTDeliveryModeDTOs.GetResult>> {
+        return try {
+            val ojtDeliveryMode = ojtDeliveryModeService.deleteEntityById(id, pageable)
+            ResponseEntity.ok(ojtDeliveryMode)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+
+
+
+
+}
---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/impl/domain/service/OJTDeliveryModeServiceImpl.kt
@@ -0,0 +1,52 @@
+package com.mmcm.projectocp.backend.spring.impl.domain.service
+
+import com.mmcm.projectocp.backend.spring.application.dto.OJTDeliveryModeDTOs
+import com.mmcm.projectocp.backend.spring.application.mapper.OJTDeliveryModeMapper
+import com.mmcm.projectocp.backend.spring.domain.repository.OJTDeliveryModeRepository
+import com.mmcm.projectocp.backend.spring.domain.service.OJTDeliveryModeService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class OJTDeliveryModeServiceImpl(
+    private val ojtDeliveryModeRepository: OJTDeliveryModeRepository,
+    private val ojtDeliveryModeMapper: OJTDeliveryModeMapper,
+) : OJTDeliveryModeService {
+    override fun getEntities(pageable: Pageable): Page<OJTDeliveryModeDTOs.GetResult> {
+        return ojtDeliveryModeRepository.findAll(pageable).map { ojtDeliveryModeMapper.toGetResult(it) }
+    }
+
+    override fun getEntityById(id: String, pageable: Pageable): Page<OJTDeliveryModeDTOs.GetResult> {
+        val ojtDeliveryMode = ojtDeliveryModeRepository.findById(id, pageable)
+        return ojtDeliveryMode.map { ojtDeliveryModeMapper.toGetResult(it) }
+    }
+
+    override fun createEntity(
+        entityRequest: OJTDeliveryModeDTOs.PostRequest,
+        pageable: Pageable
+    ): Page<OJTDeliveryModeDTOs.GetResult> {
+        val ojtDeliveryModeId = UUID.randomUUID().toString()
+        val savedOJTDeliveryMode = ojtDeliveryModeRepository.save(ojtDeliveryModeMapper.createEntity(ojtDeliveryModeId, entityRequest))
+        return ojtDeliveryModeRepository.findById(savedOJTDeliveryMode.id, pageable).map { ojtDeliveryModeMapper.toGetResult(it) }
+
+
+    }
+
+    override fun updateEntityById(
+        id: String,
+        entityRequest: OJTDeliveryModeDTOs.PutRequest,
+        pageable: Pageable
+    ): Page<OJTDeliveryModeDTOs.GetResult> {
+        val currentOJTDeliveryMode = ojtDeliveryModeRepository.findById(id).get()
+        ojtDeliveryModeRepository.save(ojtDeliveryModeMapper.updateEntity(currentOJTDeliveryMode, entityRequest))
+        return ojtDeliveryModeRepository.findById(id, pageable).map { ojtDeliveryModeMapper.toGetResult(it) }
+    }
+
+    override fun deleteEntityById(id: String, pageable: Pageable): Page<OJTDeliveryModeDTOs.GetResult> {
+        val ojtDeliveryMode = ojtDeliveryModeRepository.findById(id).get()
+        ojtDeliveryModeRepository.delete(ojtDeliveryMode)
+        return ojtDeliveryModeRepository.findAll(pageable).map { ojtDeliveryModeMapper.toGetResult(it) }
+    }
+}
