59512fbc941ebbfcaba0e4d70aa169583e4e3afe
Prince Oncada <prince.oncada@gmail.com>
2023-09-24 19:03:22 +0800

    Authentication: refactored UserPrincipal -- concatenated ROLE_ to the string role of the user before adding it as GrantedAuthority

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/service/UserPrincipal.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/service/UserPrincipal.kt
@@ -1,22 +1,21 @@
-package com.mmcm.projectocp.backend.spring.domain.service
+package com.mmcm.projectocp.backend.spring.config.service
 
 import com.mmcm.projectocp.backend.spring.domain.model.UserRole
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class UserPrincipal(
-    private val userRole: UserRole
+    private val userRole: UserRole,
 ): UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         val authorities: MutableCollection<GrantedAuthority> = mutableListOf()
-        authorities.add(GrantedAuthority { userRole.role.name })
+        authorities.add(GrantedAuthority { "ROLE_${userRole.role.name}" })
         return authorities
     }
 
     override fun getPassword(): String {
         return ""
     }
-
 
     override fun getUsername(): String {
         return userRole.user.email
