5723a90cdc255a0ee3eb36d3430640866b8f8486
Prince Oncada <prince.oncada@gmail.com>
2023-09-24 08:22:22 +0800

    Refactor: changed everything by adding an interface and new functions.

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/impl/domain/service/UserRoleServiceImpl.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/impl/domain/service/UserRoleServiceImpl.kt
@@ -1,22 +1,74 @@
 package com.mmcm.projectocp.backend.spring.impl.domain.service
 
+import com.mmcm.projectocp.backend.spring.application.dto.UserRoleDTOs
+import com.mmcm.projectocp.backend.spring.application.mapper.UserRoleMapper
 import com.mmcm.projectocp.backend.spring.domain.model.UserRole
 import com.mmcm.projectocp.backend.spring.domain.repository.UserRepository
 import com.mmcm.projectocp.backend.spring.domain.repository.UserRoleRepository
 import com.mmcm.projectocp.backend.spring.domain.service.UserRoleService
+import jakarta.transaction.Transactional
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserRoleServiceImpl(
+        private val userRoleRepository: UserRoleRepository,
         private val userRepository: UserRepository,
-        private val userRoleRepository: UserRoleRepository
+        private val userRoleMapper: UserRoleMapper
 ): UserRoleService {
-    override fun isAdmin(userId: String): Boolean {
+    override fun getEntities(
+        pageable: Pageable
+    ): Page<UserRoleDTOs.GetResult> {
+        return userRoleRepository.findAll(pageable).map { userRoleMapper.toGetResult(it) }
+    }
+
+    override fun getEntityById(
+        id: String,
+        pageable: Pageable
+    ): Page<UserRoleDTOs.GetResult> {
+        return userRoleRepository.findById(id, pageable).map { userRoleMapper.toGetResult(it) }
+    }
+
+    @Transactional
+    override fun createEntity(
+        entityRequest: UserRoleDTOs.PostRequest,
+        pageable: Pageable
+    ): Page<UserRoleDTOs.GetResult> {
+        val userRoleId = UUID.randomUUID().toString()
+        val savedUserRole = userRoleRepository.save(userRoleMapper.createEntity(userRoleId, entityRequest))
+        return userRoleRepository.findById(savedUserRole.id, pageable).map { userRoleMapper.toGetResult(it) }
+    }
+
+    override fun updateEntityById(
+        id: String,
+        entityRequest: UserRoleDTOs.PutRequest,
+        pageable: Pageable
+    ): Page<UserRoleDTOs.GetResult> {
+        val currentUserRole = userRoleRepository.findById(id).get()
+        val savedUserRole = userRoleRepository.save(userRoleMapper.updateEntity(currentUserRole, entityRequest))
+        return userRoleRepository.findById(savedUserRole.id, pageable).map { userRoleMapper.toGetResult(it) }
+    }
+
+    override fun deleteEntityById(
+        id: String,
+        pageable: Pageable
+    ): Page<UserRoleDTOs.GetResult> {
+        val userRole = userRoleRepository.findById(id).get()
+        userRoleRepository.delete(userRole)
+        return userRoleRepository.findAll(pageable).map { userRoleMapper.toGetResult(it) }
+    }
+
+    override fun isAdmin(
+        userId: String
+    ): Boolean {
         return userRoleRepository.findByUserId(userId).role.name == "admin"
     }
 
-    override fun findByEmail(email: String): UserRole {
-        val userId = userRepository.findByEmail(email).id
-        return userRoleRepository.findByUserId(userId)
+    override fun findByEmail(email: String): Optional<UserRole> {
+        val userId = userRepository.findByEmail(email).get().id
+        return userRoleRepository.findById(userId)
     }
 }
