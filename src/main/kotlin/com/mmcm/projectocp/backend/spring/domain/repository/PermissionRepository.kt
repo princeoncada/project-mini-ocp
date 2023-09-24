e8c49e6ff5ace3c9ec5f143e605ec8b756bf5b33
Prince Oncada <prince.oncada@gmail.com>
2023-09-24 08:15:40 +0800

    Repository: added findByName: Optional<Permission> in PermissionRepository.kt

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PermissionRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PermissionRepository.kt
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface PermissionRepository: JpaRepository<Permission, String> {
     fun findById(id: String, pageable: Pageable): Page<Permission>
+    fun findByName(name: String?): Optional<Permission>
 }
