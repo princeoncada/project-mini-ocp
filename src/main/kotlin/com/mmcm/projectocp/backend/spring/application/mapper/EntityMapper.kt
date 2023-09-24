4084e7e85caa9d340bf467d1d833a1081d244b9c
Prince Oncada <pgsOncada@mcm.edu.ph>
2023-09-24 21:07:05 +0800

    Update EntityMapper.kt
    fixed spacing issues

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/application/mapper/EntityMapper.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/application/mapper/EntityMapper.kt
@@ -1,7 +1,5 @@
 package com.mmcm.projectocp.backend.spring.application.mapper
 
-
-
 interface EntityMapper<Entity, GetResult, PostRequest, PutRequest> {
     fun toGetResult(entity: Entity): GetResult
     fun createEntity(id: String, entityRequest: PostRequest): Entity
