f6cda4cdf932df5f3e15d8c646074f59fe6b586b
Prince Oncada <pgsOncada@mcm.edu.ph>
2023-09-24 21:00:12 +0800

    Update CompanyCategoryDTOs.kt
    fixed spacing issues

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/application/dto/CompanyCategoryDTOs.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/application/dto/CompanyCategoryDTOs.kt
@@ -4,16 +4,13 @@ class CompanyCategoryDTOs {
     data class GetResult(
         val id: String,
         val name: String,
-
-        )
+    )
 
     data class PostRequest(
         val name: String,
-
-        )
+    )
 
     data class PutRequest(
         val name: String?,
-
-        )
+    )
 }
