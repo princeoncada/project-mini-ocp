99ae2d0948295679ab8bc6c2919190b6f1237227
KuuShyn <magalonakaj@gmail.com>
2023-09-24 20:24:44 +0800

    Build: created @MeetingStatus Services and Controller

---
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/application/mapper/MeetingStatusMapper.kt
@@ -0,0 +1,35 @@
+package com.mmcm.projectocp.backend.spring.application.mapper
+
+import com.mmcm.projectocp.backend.spring.application.dto.MeetingStatusDTOs
+import com.mmcm.projectocp.backend.spring.domain.model.MeetingStatus
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+@Component
+class MeetingStatusMapper : EntityMapper<MeetingStatus, MeetingStatusDTOs.GetResult, MeetingStatusDTOs.PostRequest, MeetingStatusDTOs.PutRequest> {
+    override fun toGetResult(entity: MeetingStatus): MeetingStatusDTOs.GetResult {
+        return MeetingStatusDTOs.GetResult(
+            id = entity.id,
+            name = entity.name
+        )
+    }
+
+    override fun createEntity(id: String, entityRequest: MeetingStatusDTOs.PostRequest): MeetingStatus {
+        return MeetingStatus(
+            id = id,
+            name = entityRequest.name,
+            createdAt = Instant.now(),
+            updatedAt = Instant.now()
+        )
+    }
+
+    override fun updateEntity(entity: MeetingStatus, entityRequest: MeetingStatusDTOs.PutRequest): MeetingStatus {
+        return MeetingStatus(
+            id = entity.id,
+            name = entityRequest.name?: entity.name,
+            createdAt = entity.createdAt,
+            updatedAt = Instant.now()
+
+        )
+    }
+}
--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/MeetingStatusRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/MeetingStatusRepository.kt
@@ -1,10 +1,14 @@
 package com.mmcm.projectocp.backend.spring.domain.repository
 
 import com.mmcm.projectocp.backend.spring.domain.model.MeetingStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface MeetingStatusRepository : JpaRepository<MeetingStatus, String> {
     // You can add custom query methods here if needed
+    fun findById(id: String, pageable: Pageable): Page<MeetingStatus>
+
 }
