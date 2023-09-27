4862dbf21d3cbce10371d062ed45d8d3e3477347
Prince Oncada <prince.oncada@gmail.com>
2023-09-27 18:37:29 +0800

    Authentication: created JwtConfiguration.kt which include a KeyGenerator for secretKey

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/config/SecurityConfig.kt
@@ -1,23 +1,31 @@
 package com.mmcm.projectocp.backend.spring.config
 
-import com.mmcm.projectocp.backend.spring.config.service.CustomOidcUserService
-import com.mmcm.projectocp.backend.spring.config.service.CustomUserDetailsServiceImpl
-import com.mmcm.projectocp.backend.spring.config.service.JwtAuthenticationFilter
-import com.mmcm.projectocp.backend.spring.config.service.JwtService
+import com.mmcm.projectocp.backend.spring.config.service.*
+import com.mmcm.projectocp.backend.spring.domain.repository.RefreshTokenRepository
+import com.mmcm.projectocp.backend.spring.domain.repository.UserRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val jwtService: JwtService,
-    private val userPrincipalService: CustomUserDetailsServiceImpl
+    private val userPrincipalService: CustomUserDetailsServiceImpl,
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val userRepository: UserRepository
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -27,15 +35,47 @@ class SecurityConfig(
 //                authorize("/api/users/**", hasRole("admin"))
                 authorize(anyRequest, authenticated)
             }
+            sessionManagement {
+                SessionCreationPolicy.STATELESS
+            }
             oauth2Login {
                 userInfoEndpoint {
                     oidcUserService = oidcUserService()
                 }
-                defaultSuccessUrl("/authenticate", true)
+                defaultSuccessUrl("/oauth2/callback", true)
             }
-            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtAuthenticationFilter(jwtService, userPrincipalService))
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(
+                JwtAuthenticationFilter(
+                    jwtService,
+                    userPrincipalService,
+                    refreshTokenRepository
+                )
+            )
+            logout {
+                logoutUrl = "/logout"
+                logoutSuccessHandler = CustomLogoutSuccessHandler(userRepository, jwtService)
+                permitAll()
+            }
         }
         return http.build()
+    }
+
+    class CustomLogoutSuccessHandler(
+        private val userRepository: UserRepository,
+        private val jwtService: JwtService
+    ): LogoutSuccessHandler{
+        override fun onLogoutSuccess(
+            request: HttpServletRequest?,
+            response: HttpServletResponse?,
+            authentication: Authentication?
+        ) {
+            userRepository.findByEmail(authentication?.name).get().let { user ->
+                jwtService.revokeRefreshToken(user.id)
+            }
+            jwtService.clearAccessTokenCookie(response!!)
+            SecurityContextHolder.clearContext()
+            response.sendRedirect("/login?logout")
+        }
     }
 
     @Bean
