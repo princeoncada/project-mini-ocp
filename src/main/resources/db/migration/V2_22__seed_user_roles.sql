c5f03a76ecb790c2cb562eed617ac4c19eba6504
Prince Oncada <prince.oncada@gmail.com>
2023-09-27 16:51:43 +0800

    Migration: included insert of email mrlotzo@gmail.com and set it as admin in user roles. created a new table tbl_refresh_tokens for jwt security improvement

--- src/main/resources/db/migration/V2_22__seed_user_roles.sql
+++ src/main/resources/db/migration/V2_22__seed_user_roles.sql
@@ -5,6 +5,12 @@
 SET @USER_ID = (SELECT id FROM tbl_users WHERE first_name = 'Dummy');
 SET @ROLE_ID = (SELECT id FROM tbl_roles WHERE `name` = 'wica');
 
+SET @PRINCE_USER_ID = (SELECT id FROM tbl_users WHERE first_name = 'Prince');
+SET @ADMIN_ROLE_ID = (SELECT id FROM tbl_roles WHERE `name` = 'admin');
+
 -- Insert Entry
 INSERT INTO tbl_user_roles (id, user_id, role_id)
 VALUES (UUID(), @USER_ID, @ROLE_ID);
+
+INSERT INTO tbl_user_roles (id, user_id, role_id)
+VALUES (UUID(), @PRINCE_USER_ID, @ADMIN_ROLE_ID);
