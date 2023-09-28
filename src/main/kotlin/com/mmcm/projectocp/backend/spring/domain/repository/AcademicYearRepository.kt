7a6c9b63e481da97945aadbb5b430adeb65ce95e
Prince Oncada <prince.oncada@gmail.com>
2023-09-28 19:00:09 +0800

    Repository: added findByYearToAndYearFrom to AcademicYearRepository

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/AcademicYearRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/AcademicYearRepository.kt
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface AcademicYearRepository: JpaRepository<AcademicYear, String> {
     fun findById(id: String, pageable: Pageable): Page<AcademicYear>
     fun findByYearFrom(yearFrom: Int): AcademicYear
+    fun findByYearToAndYearFrom(yearTo: Int?, yearFrom: Int?): Optional<AcademicYear>
 }
