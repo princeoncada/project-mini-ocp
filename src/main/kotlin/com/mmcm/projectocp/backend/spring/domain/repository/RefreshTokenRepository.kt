2e7c931765a51edb3f5f508b195dc9e8a36d1a27
Prince Oncada <prince.oncada@gmail.com>
2023-09-28 09:59:26 +0800

    Refactor: changed findByUserId return type to optional refresh token

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/RefreshTokenRepository.kt
@@ -4,8 +4,9 @@ import com.mmcm.projectocp.backend.spring.domain.model.RefreshToken
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface RefreshTokenRepository: JpaRepository<RefreshToken, String> {
     fun findById(id: String, pageable: Pageable): Page<RefreshToken>
-    fun findByUserId(userId: String): RefreshToken
+    fun findByUserId(userId: String): Optional<RefreshToken>
 }
