fd2b5dac1f9f3c574aeaebdc4202bd0f3b2ae527
Prince Oncada <prince.oncada@gmail.com>
2023-10-09 19:03:26 +0800

    Repository: added findByName for MoaPositionRepository

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/MoaPositionRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/MoaPositionRepository.kt
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface MoaPositionRepository: JpaRepository<MoaPosition, String> {
     fun findById(id: String, pageable: Pageable): Page<MoaPosition>
+    fun findByName(name: String?): Optional<MoaPosition>
 }
