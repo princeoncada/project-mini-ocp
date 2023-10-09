e11ae62b8545f81b9e105cdb66239739c58622ce
Prince Oncada <prince.oncada@gmail.com>
2023-10-09 19:03:45 +0800

    Repository: added findByType for PartnershipTypeRepository

--- src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PartnershipTypeRepository.kt
+++ src/main/kotlin/com/mmcm/projectocp/backend/spring/domain/repository/PartnershipTypeRepository.kt
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface PartnershipTypeRepository: JpaRepository<PartnershipType, String> {
     fun findById(id: String, pageable: Pageable): Page<PartnershipType>
+    fun findByType(type: String?): Optional<PartnershipType>
 }
